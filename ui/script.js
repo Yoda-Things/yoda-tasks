604cabbc5c3c6757e3fd7a5211b43e79efecb30a
CigarettesDaGuxe <57046735+Cigarettes1Tamagucci@users.noreply.github.com>
2025-03-06 19:56:24 +0000

    mudanças

--- ui/script.js
+++ ui/script.js
@@ -27,31 +27,35 @@ function updateTask() {
 }
 
 function appendProgTaskUI(title, count, maxcount, icon, color) {
-    if (title !== null) currentTitle = title;
-    if (maxcount !== undefined) currentMaxCount = maxcount;
-    if (icon !== undefined) currentIcon = icon;
-    if (color !== undefined) currentColor = color;
-    const container = document.querySelector(".prog-task-container");
-    container.style.display = 'none'
-    container.innerHTML = "";
-    container.style.display = 'flex'
-    container.classList.remove("fade-in");
-    void container.offsetWidth;
-    container.classList.add("fade-in");
-    const progressBarWidth = 16.5 * (count / currentMaxCount);
-    console.log(progressBarWidth)
-    const newContent = `
-        <div class="left-part-cont">
-            <i class="${currentIcon}"></i>
-        </div>
-        <div class="right-part-cont">
-            <div class="prog-title">${currentTitle}</div>
-            <div class="progbar-container">
-                <div class="progbar-progress" style="width: ${progressBarWidth}vw"></div>
+    if (count !== maxcount) {
+        if (title !== null) currentTitle = title;
+        if (maxcount !== undefined) currentMaxCount = maxcount;
+        if (icon !== undefined) currentIcon = icon;
+        if (color !== undefined) currentColor = color;
+        const container = document.querySelector(".prog-task-container");
+        container.style.display = 'none'
+        container.innerHTML = "";
+        container.style.display = 'flex'
+        container.classList.remove("fade-in");
+        void container.offsetWidth;
+        container.classList.add("fade-in");
+        const progressBarWidth = 16.5 * (count / currentMaxCount);
+        const newContent = `
+            <div class="left-part-cont">
+                <i class="${currentIcon}"></i>
             </div>
-            <div class="task-info">${count}/${currentMaxCount}</div>
-        </div>
-    `;
+            <div class="right-part-cont">
+                <div class="prog-title">${currentTitle}</div>
+                <div class="progbar-container">
+                    <div class="progbar-progress" style="width: ${progressBarWidth}vw"></div>
+                </div>
+                <div class="task-info">${count}/${currentMaxCount}</div>
+            </div>
+        `;
 
-    container.innerHTML = newContent;
+        container.innerHTML = newContent;
+    } else {
+        container.innerHTML = ""
+        container.style.display = 'none'
+    }
 }
